4403fe96f545db49189d6ef71c65f2115e1172fa
homero <homerogazze1015@gmail.com>
2022-10-04 17:04:34 -0300

    [FIX] Fixing Icons and Image Not Found Errors

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import style from './App.css'
 import alanBtn from '@alan-ai/alan-sdk-web'
 
 
+// Components
+import NewsCards from './Components/NewsCards/NewsCards'
+
+
 function App() {
 
   const [ newsArticles, setNewsArticles ] = useState([])
@@ -19,10 +23,11 @@ function App() {
     })
 
   }, [])
-
+  
   return (
     <div >
-      Alan AI News Appliclation
+      <h1>Alan AI News Appliclation</h1>
+      <NewsCards articles={newsArticles} />
     </div>
   )
 }

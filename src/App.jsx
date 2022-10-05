5bdf7756ed5d335d3486a08299ce19c2d3379081
homero <homerogazze1015@gmail.com>
2022-10-05 12:11:32 -0300

    [NEW] Automatic Open Articles Added (Feature)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from 'react'
+// Hooks
+import { useState, useEffect, useRef } from 'react'
 
+// Alan AI
 import alanBtn from '@alan-ai/alan-sdk-web'
-
 
 // Components
 import NewsCards from './Components/NewsCards/NewsCards'
@@ -9,23 +10,42 @@ import NewsCards from './Components/NewsCards/NewsCards'
 // Styles
 import useStyles from './Styles.js'
 
+// Others
+import wordToNumber from 'word-to-numbers';
+
 function App() {
 
   const classes = useStyles()
   const [ newsArticles, setNewsArticles ] = useState([])
   const [ activeArticle, setActiveArticle] = useState(-1)
 
+  const alanBtnRef = useRef({}).current;
+
   useEffect(() => {
 
-    alanBtn({
+    alanBtnRef.btnInstance = alanBtn({
       key: import.meta.env.VITE_ALAN_KEY,  
-      onCommand: ({ command, articles }) => {
+      onCommand: ({ command, articles, number }) => {
           if(command === 'newHeadlines') {
             setNewsArticles(articles)
             setActiveArticle(-1)
           }
           else if(command === 'highlight') {
             setActiveArticle((prev) => prev + 1);
+          }
+          else if(command === 'open') {
+            const parsedNumber = number.length > 2 ? wordToNumber(number) : number;
+            const article = articles[parsedNumber - 1];
+  
+            if (parsedNumber > articles.length) {
+              alanBtnRef.btnInstance.playText('Please try with a lower number.');
+            } else if (article) {
+              window.open(article.url, '_blank');
+              alanBtnRef.btnInstance.playText('Opening...');
+            } else {
+              alanBtnRef.btnInstance.playText('Please try that again...');
+            }
+            
           }
       }
     })

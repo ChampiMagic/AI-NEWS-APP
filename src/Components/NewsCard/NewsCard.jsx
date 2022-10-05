f7366a03963da8690ea85cdeedd7269656a3cc08
homero <homerogazze1015@gmail.com>
2022-10-05 09:40:19 -0300

    [NEW] Automatic Scrool Added (Feature)

--- src/Components/NewsCard/NewsCard.jsx
+++ src/Components/NewsCard/NewsCard.jsx
@@ -1,3 +1,6 @@
+// Hooks
+import { useEffect, useState, createRef } from "react";
+
 // Components
 import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from "@material-ui/core";
 
@@ -10,9 +13,23 @@ import classNames from "classnames";
 const NewCard = ({ article: { description, publishedAt, source, title, url, urlToImage}, i, activeArticle }) => {
 
     const classes = useStyles();
+
+    const [ elRefs, setElRef ] = useState([])
+
+    const scrollToRef = (ref) => window.scroll(0, ref.current.offsetTop - 50);
     
+    useEffect(() => {
+        setElRef((refs) => Array(20).fill().map((_, j) => refs[j] || createRef()))
+    }, []);
+
+    useEffect(() => {
+        if(i === activeArticle && elRefs[activeArticle]) {
+             scrollToRef(elRefs[activeArticle]);
+        }
+    }, [i, activeArticle, elRefs])
+
     return (
-        <Card className={classNames(classes.card, activeArticle === i? classes.activeCard : null)}>
+        <Card ref={elRefs[i]} className={classNames(classes.card, activeArticle === i? classes.activeCard : null)}>
             <CardActionArea href={url} target="_blank">
                 <CardMedia className={classes.media} image={ urlToImage ||  "https://images.unsplash.com/photo-1585829365295-ab7cd400c167?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cGVyaW9kaXN0YXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"}/>
                 <div className={classes.details}>
